68c83681c89a7ef0719517501daa72e281ae56e3
Cu7ious <328@holbertonschool.com>
2018-08-14 02:57:08 +0000

    Solution for the task #0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module that creates very basic Flask app
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root_route():
+    """ The root aoute.
+        Sends the "Hello HBNB!" text
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
